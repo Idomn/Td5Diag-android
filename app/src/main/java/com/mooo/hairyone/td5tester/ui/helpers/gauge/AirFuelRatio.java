dc857de8cc4a70a813abc2607fb678fdf60ee68d
Idomn <ido.minsky@gmail.com>
2025-08-21 23:21:53 +0300

    Replace LibGauge with local Td5Gauge

--- app/src/main/java/com/mooo/hairyone/td5tester/ui/helpers/gauge/AirFuelRatio.java
+++ app/src/main/java/com/mooo/hairyone/td5tester/ui/helpers/gauge/AirFuelRatio.java
@@ -1,64 +1,32 @@
 package com.mooo.hairyone.td5tester.ui.helpers.gauge;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.util.AttributeSet;
 
 import com.mooo.hairyone.td5tester.R;
 import com.mooo.hairyone.td5tester.ui.helpers.Td5Gauge;
 
-public class AirFuelRatio extends Td5Gauge
-{
+public class AirFuelRatio extends Td5Gauge {
 
-
-    public AirFuelRatio(Context context, AttributeSet attrs)
-    {
+    public AirFuelRatio(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-        this.init();
+        init();
     }
 
+    protected void init() {
+        int min = getContext().getResources().getInteger(R.integer.fueling_airFuelRatio_gaugeMin);
+        int max = getContext().getResources().getInteger(R.integer.fueling_airFuelRatio_gaugeMax);
 
-    protected void init()
-    {
-        int lGaugeValueMin
-                = getContext().getResources().getInteger(
-                        R.integer.fueling_airFuelRatio_gaugeMin);
-        int lGaugeValueMax
-                = getContext().getResources().getInteger(
-                        R.integer.fueling_airFuelRatio_gaugeMax);
-
-
-        this.setGaugeName(getContext().getResources().getString(
-                R.string.air_fuel_ratio_short));
-
-        this.setGraduationMin( lGaugeValueMin );
-        this.setGraduationMax( lGaugeValueMax );
-
-//        this.setValue(lGaugeValueMax);
-        this.setValue(88f);
-
-
-        this.getValueDisplay().setValueDisplayFormat("%2.0f:1");
-        this.getValueDisplay().setUnitText(getContext().getResources().getString(
-                R.string.air_fuel_ratio_unit));
-
-
-        this.setGraduationCountMajor((int) ((lGaugeValueMax - lGaugeValueMin) / 5.0f) + 1);
-        //this.getDial().getGraduationsList().get(EGraduationsIndex.GRAD_MAJOR.getIndex())
-        //       .setValuesTextFormat("%.0f");
-        this.getDial().getGraduationsList().get(EGraduationsIndex.GRAD_MAJOR.getIndex())
-                .setGraduationColor(Color.LTGRAY);
-
-        this.setGraduationCountMinor((int) ((lGaugeValueMax - lGaugeValueMin) / 2.5f) + 1);
-        this.getDial().getGraduationsList().get(EGraduationsIndex.GRAD_MINOR.getIndex())
-                .setValuesTextFormat("");
-        this.getDial().getGraduationsList().get(EGraduationsIndex.GRAD_MINOR.getIndex())
-                .setLinesLengthFactor(5.0f);
-
-
-        /*
-            Sections definition
-         */
+        setGaugeName(getContext().getResources().getString(R.string.air_fuel_ratio_short));
+        setGraduationMin(min);
+        setGraduationMax(max);
+        setValue(max);
+        setValueDisplayFormat("%2.0f:1");
+        setUnitText(getContext().getResources().getString(R.string.air_fuel_ratio_unit));
+        setGraduationCountMajor((int) ((max - min) / 5.0f) + 1);
+        setGraduationCountMinor((int) ((max - min) / 2.5f) + 1);
+        setGraduationMajorFormat("%.0f");
+        setGraduationMinorFormat("");
     }
 }
+
--- app/src/main/java/com/mooo/hairyone/td5tester/ui/helpers/gauge/EngineRPM.java
+++ app/src/main/java/com/mooo/hairyone/td5tester/ui/helpers/gauge/EngineRPM.java
@@ -1,8 +1,6 @@
 package com.mooo.hairyone.td5tester.ui.helpers.gauge;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.util.AttributeSet;
 
 import com.mooo.hairyone.td5tester.R;
@@ -10,98 +8,49 @@ import com.mooo.hairyone.td5tester.ui.helpers.Td5Gauge;
 
 public class EngineRPM extends Td5Gauge {
 
-
-    public EngineRPM(Context context, AttributeSet attrs)
-    {
+    public EngineRPM(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-        this.init();
+        init();
     }
 
+    protected void init() {
+        int min = getContext().getResources().getInteger(R.integer.engine_rpm_gaugeMin);
+        int max = getContext().getResources().getInteger(R.integer.engine_rpm_gaugeMax);
 
-    protected void init()
-    {
-        int lGaugeValueMin
-                = getContext().getResources().getInteger( R.integer.engine_rpm_gaugeMin);
-        int lGaugeValueMax
-                = getContext().getResources().getInteger( R.integer.engine_rpm_gaugeMax);
-
-
-        this.setGraduationMin( lGaugeValueMin );
-        this.setGraduationMax( lGaugeValueMax );
-
-        this.setGaugeName(getContext().getResources().getString(R.string.engine_rpm_short));
-        this.setValue(8888.0f);
-
-
-        this.getValueDisplay().setValueDisplayFormat("%4.0f");
-        this.getValueDisplay().setUnitText("RPM");
-
-
-        this.setDialValueRangeFactor(1000);
-
-        this.setGraduationCountMajor( ((lGaugeValueMax - lGaugeValueMin) / 1000) + 1);
-        this.getDial().getGraduationsList().get(EGraduationsIndex.GRAD_MAJOR.getIndex())
-                .setValuesTextFormat("%.0f");
-
-        this.setGraduationCountMinor( ((lGaugeValueMax - lGaugeValueMin) / 1000) + 1);
-
-//        this.setMarksNumber( ((lGaugeValueMax - lGaugeValueMin) / 1000) + 1 - 2);
-
-
-        /*
-            Sections definition
-         */
-
-        /* "Low" section */
-        this.section_add(
-                lGaugeValueMin,
-                getContext().getResources().getInteger( R.integer.engine_rpm_idle_low),
-                getContext().getResources().getColor( R.color.valueInc_invalid ) );
-
-        /* "Idle" section */
-        this.section_add(
-                //lGaugeValueMin,
-                getContext().getResources().getInteger( R.integer.engine_rpm_idle_low),
-                getContext().getResources().getInteger( R.integer.engine_rpm_idle_high),
-                getContext().getResources().getColor( R.color.valueInc_veryLow ) );
-
-        /* low RPM torque */
-        this.section_add(
-                getContext().getResources().getInteger( R.integer.engine_rpm_idle_high),
-                getContext().getResources().getInteger( R.integer.engine_rpm_torque80_low),
-                getContext().getResources().getColor( R.color.valueInc_low ) );
-
-        /* 80% torque lower */
-        this.section_add(
-                getContext().getResources().getInteger( R.integer.engine_rpm_torque80_low),
-                getContext().getResources().getInteger( R.integer.engine_rpm_torque90),
-                getContext().getResources().getColor( R.color.valueInc_ok_low ) );
-
-        /* High torque */
-        this.section_add(
-                getContext().getResources().getInteger( R.integer.engine_rpm_torque90),
-                getContext().getResources().getInteger( R.integer.engine_rpm_torqueMax),
-//                Color.GREEN
-                getContext().getResources().getColor( R.color.valueInc_ok )
-        );
-
-        /* 80% torque higher */
-        this.section_add(
-                getContext().getResources().getInteger( R.integer.engine_rpm_torqueMax),
-                getContext().getResources().getInteger( R.integer.engine_rpm_torque80_high),
-                getContext().getResources().getColor( R.color.valueInc_ok_high ) );
-
-        /* 80% torque to max governed speed */
-        this.section_add(
-                getContext().getResources().getInteger( R.integer.engine_rpm_torque80_high),
-                getContext().getResources().getInteger( R.integer.engine_rpm_maxGovernedSpeed),
-                getContext().getResources().getColor( R.color.valueInc_high ) );
-
-        /* "Maximum governed speed" section */
-        this.section_add(
-                getContext().getResources().getInteger( R.integer.engine_rpm_maxGovernedSpeed),
-                lGaugeValueMax,
-                getContext().getResources().getColor( R.color.valueInc_veryHigh ) );
+        setGraduationMin(min);
+        setGraduationMax(max);
+        setGaugeName(getContext().getResources().getString(R.string.engine_rpm_short));
+        setValue(max);
+        setValueDisplayFormat("%4.0f");
+        setUnitText("RPM");
+        setDialValueRangeFactor(1000);
+        setGraduationCountMajor(((max - min) / 1000) + 1);
+        setGraduationCountMinor(((max - min) / 1000) + 1);
+        setGraduationMajorFormat("%.0f");
+        section_add(min,
+                getContext().getResources().getInteger(R.integer.engine_rpm_idle_low),
+                getContext().getResources().getColor(R.color.valueInc_invalid));
+        section_add(getContext().getResources().getInteger(R.integer.engine_rpm_idle_low),
+                getContext().getResources().getInteger(R.integer.engine_rpm_idle_high),
+                getContext().getResources().getColor(R.color.valueInc_veryLow));
+        section_add(getContext().getResources().getInteger(R.integer.engine_rpm_idle_high),
+                getContext().getResources().getInteger(R.integer.engine_rpm_torque80_low),
+                getContext().getResources().getColor(R.color.valueInc_low));
+        section_add(getContext().getResources().getInteger(R.integer.engine_rpm_torque80_low),
+                getContext().getResources().getInteger(R.integer.engine_rpm_torque90),
+                getContext().getResources().getColor(R.color.valueInc_ok_low));
+        section_add(getContext().getResources().getInteger(R.integer.engine_rpm_torque90),
+                getContext().getResources().getInteger(R.integer.engine_rpm_torqueMax),
+                getContext().getResources().getColor(R.color.valueInc_ok));
+        section_add(getContext().getResources().getInteger(R.integer.engine_rpm_torqueMax),
+                getContext().getResources().getInteger(R.integer.engine_rpm_torque80_high),
+                getContext().getResources().getColor(R.color.valueInc_ok_high));
+        section_add(getContext().getResources().getInteger(R.integer.engine_rpm_torque80_high),
+                getContext().getResources().getInteger(R.integer.engine_rpm_maxGovernedSpeed),
+                getContext().getResources().getColor(R.color.valueInc_high));
+        section_add(getContext().getResources().getInteger(R.integer.engine_rpm_maxGovernedSpeed),
+                max,
+                getContext().getResources().getColor(R.color.valueInc_veryHigh));
     }
 }
+
